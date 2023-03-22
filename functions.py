f48b95907c5703dd4dcf16e926f78981f9cd92e4
Daniel Meseguer <dmesmun@epsg.upv.es>
2023-03-22 11:12:24 +0100

    commit 5

--- functions.py
+++ functions.py
@@ -1,18 +1,31 @@
-import csv
+
 def read_data(_archivo):
    # try:
-
+        import csv
         with open(_archivo, 'r') as file:
             reader = csv.reader(file)
             i = 1
             dic = dict()
+            flag=1
+            claves=list()
             for row in reader:
-                dic2 = dict()
-                for atribute in row:
-                    if(len(dic2)==13):
-                        dic2.update({atribute:row[atribute]})
-                dic.update({"dato{0}".format(i): dic2})
-                i = i + 1
+                if flag==0:
+                    dic2 = dict()
+                    p=0
+                    for atribute in row:
+                        dic2.update({claves[p]: atribute})
+                        p=p+1
+                    #if dic2.get(' ')==:
+                    dic.update({"dato{0}".format(i): dic2})
+                    i = i + 1
+
+                else:
+                    flag=0
+                    for atribute in row:
+                        claves.append(atribute)
+
+
+
 
             print(dic)
 
