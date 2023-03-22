ab267530327b6e94491ecaa40c00c6f1b6625d48
Daniel Meseguer <dmesmun@epsg.upv.es>
2023-03-22 10:46:21 +0100

    commit 4

--- functions.py
+++ functions.py
@@ -1,19 +1,23 @@
 import csv
 def read_data(_archivo):
-    try:
+   # try:
 
         with open(_archivo, 'r') as file:
             reader = csv.reader(file)
+            i = 1
+            dic = dict()
+            for row in reader:
+                dic2 = dict()
+                for atribute in row:
+                    if(len(dic2)==13):
+                        dic2.update({atribute:row[atribute]})
+                dic.update({"dato{0}".format(i): dic2})
+                i = i + 1
 
-    except:
+            print(dic)
+
+
+    #except:
         print("el nombre del archivo es incorrecto// error lectura del archivo")
-    else:
-        i = 1
-        dic = {}
-        for row in reader:
-            dic.update({"dato{0}".format(i): row})
-            i = i + 1
 
-        print(dic)
 
-"""
